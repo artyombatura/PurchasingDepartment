0aef356aa55be50379585e6b21539608dd7f3e20
Artsiom Batura <artyombatura@gmail.com>
2021-04-26 21:30:17 +0300

    Initial commit

---
+++ PurchasingDepartment/PresentationLayer/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  PurchasingDepartment
+//
+//  Created by Artsiom Batura on 26/04/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+}
+
